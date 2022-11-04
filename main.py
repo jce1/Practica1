d188643af5d0ddfe306fc4936e187b9cc5fbec34
jcescribano <jcescribano@uoc.edu>
2022-11-04 20:34:37 +0100

    Refactorización del codigo

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 '''
 Created on 1 nov 2022
 
-@author: anon
+@author: jcescribano
 '''
 import csv
-
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from requests import get
@@ -13,40 +12,45 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-url="https://contrataciondelestado.es/wps/portal/licitaciones"
+url = "https://contrataciondelestado.es/wps/portal/licitaciones"
+nombreFichero = 'contratacion.csv'
+pathGecko = "/usr/bin/geckodriver"
+pathFirefox = "/usr/bin/firefox"
 
 
-def open_browser():
-
-    try:
-        driver = webdriver.Firefox(executable_path="/usr/bin/geckodriver", firefox_binary='/usr/bin/firefox')
-        driver.get(url)
-        driver.find_element(By.CLASS_NAME,'divLogo').click()
-        driver.find_element(By.ID, 'viewns_Z7_AVEQAI930OBRD02JPMTPG21004_:form1:button1').click()
-        element = WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.CLASS_NAME, "cabeceraTexto")))
-        tablaResultados=driver.find_element(By.ID,'myTablaBusquedaCustom')
-        print(tablaResultados)
-        expedientes=driver.find_elements(By.ID,'tdExpediente')
-        print(expedientes)
-        tabla = driver.find_element(By.TAG_NAME, 'tbody')
-       
-        print(tabla.text)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        tbody = soup.find('tbody')
-        data = []
-        with open('contratacion.csv', 'w', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=' ',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for td in tbody.find_all('tr'):
-                row = [i.text for i in td.find_all('td')]
-                spamwriter.writerow(row)
-                data.append(row)
+def abrir_firefox():
+    return webdriver.Firefox(executable_path=pathGecko, firefox_binary=pathFirefox)
 
 
-        print("fin")
-    except TimeoutException:
-        print("Timed out waiting for page to load")
+def anadir_datos(nombre_fichero, fila_para_guardar):
+    with open(nombre_fichero, 'a', encoding='UTF8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fila_para_guardar)
+
+def recuperarInformacion():
+    navegador = abrir_firefox()
+    navegador.get(url)
+    navegador.find_element(By.CLASS_NAME, 'divLogo').click()
+    navegador.find_element(By.ID, 'viewns_Z7_AVEQAI930OBRD02JPMTPG21004_:form1:button1').click()
+    WebDriverWait(navegador, 1000).until(EC.presence_of_element_located((By.CLASS_NAME, "cabeceraTexto")))
+    soup = BeautifulSoup(navegador.page_source, 'html.parser')
+    tbody = soup.find('tbody')
+
+    for td in tbody.find_all('tr'):
+        if td.find(class_="tdExpediente"):
+            expediente = td.find(class_="tdExpediente").find_all('div')[0].text
+            descripcionExpediente = td.find(class_="tdExpediente").find_all('div')[1].text
+            listaTipoContrato = td.find(class_='tdTipoContrato').find_all('div')
+            tipoContrato = ""
+            for tc in listaTipoContrato:
+                tipoContrato = tc.text + ";" + tipoContrato
+            estado = td.find(class_='tdEstado').text
+            importe = td.find(class_='tdImporte').text
+            fechaLimite = td.find(class_='tdFechaLimite').text
+            organoContratacion = td.find(class_='tdOrganoContratacion').text
+            fila=[expediente, descripcionExpediente, tipoContrato, estado, importe, fechaLimite, organoContratacion]
+            anadir_datos(nombreFichero, fila)
+    navegador.close()
 
 
-
-open_browser()
+recuperarInformacion()
